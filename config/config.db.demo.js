dc2149039b7b351cf1ddd01a133fa8c317be4e56
haojie <haojie@whoisbest.lan>
2018-06-28 14:04:46 +0800

    add config files

---
+++ config/config.db.demo.js
@@ -0,0 +1,20 @@
+const db = {
+    clients: {
+        // clientId, 获取client实例，需要通过 app.mysql.get('clientId') 获取
+        db2: {
+            type: 'mongodb',
+            enable: false,
+            // host
+            host: 'mysql2.com',
+            // 端口号
+            port: '3307',
+            // 用户名
+            user: 'test_user',
+            // 密码
+            password: 'test_password',
+            // 数据库名
+            database: 'test',
+        }
+    }
+};
+module.exports = db;
